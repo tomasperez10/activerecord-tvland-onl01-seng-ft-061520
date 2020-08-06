d1ed008c984b0addad1a0d920cc70e5b22bab7df
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 05:58:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     actors = self.actors.collect do |actor|
-      actor = Actor.new.full_name
+      actor = Actor.new.full_name.length.text
     end
   end
 end
